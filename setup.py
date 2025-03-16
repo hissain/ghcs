f38cad520399eadca279ab331adf80761ada038c
Sazzad Hissain Khan <hissain.khan@gmail.com>
2025-03-17 00:13:30 +0600

    Fixed configs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ghcs",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["requests", "python-dotenv"],
     entry_points={"console_scripts": ["ghcs=ghcs.cli:main"]},
